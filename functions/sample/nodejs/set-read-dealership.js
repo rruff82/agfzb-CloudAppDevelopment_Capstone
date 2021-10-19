b9f33d83d98d89be9f842a712adca4d447e01061
Ryan Ruff <rruff82@gmail.com>
2021-10-19 12:06:30 -0400

    Changing cloudfuntions to use sequences with Cloudant ops instead

---
+++ functions/sample/nodejs/set-read-dealership.js
@@ -0,0 +1,55 @@
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+const QUERY_FIELDS = [
+    "id", "address", "city", "full_name", "lat", "long", "short_name", "st", "zip"
+]
+
+function query_by_state(st) {
+    return {
+        "selector": {
+            "st": {
+                "$eq": st
+            }
+        },
+        "fields": QUERY_FIELDS,
+
+    }
+}
+
+function query_by_id(id) {
+    return {
+        "selector": {
+            "id": {
+                "$eq": id
+            }
+        },
+        "fields": QUERY_FIELDS,
+    }
+}
+
+function query_all() {
+    return {
+        "selector": {
+            "id": {
+                "$gte": 0
+            }
+        },
+        "fields": QUERY_FIELDS,
+    }
+}
+
+function main(params) {
+
+    if ("STATE" in params)
+        return { "query": query_by_state(params.STATE), "include_docs": true };
+    if ("ID" in params)
+        return { "query": query_by_id(params.ID), "include_docs": true };
+    return { "query": query_all(), "include_docs": true };
+}
---
+++ functions/sample/python/set-read-review.py
@@ -0,0 +1,40 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+
+QUERY_FIELDS = ["id", "car_make", "car_model", "car_year", 
+                "dealership", "name", "purchase", "purchase_date", 
+                "review"]
+    
+def get_query_by_id(id):
+    return {"selector": {"id": {"$eq": id}}, 
+        "fields": QUERY_FIELDS}  
+ 
+
+def query_by_dealership(dealer_id):
+    return {
+        "selector": {"dealership": {"$eq": dealer_id}}, 
+        "fields": QUERY_FIELDS}
+def query_all():
+    return { 
+        "selector": {"id": {"$gte": 0}}, 
+        "fields": QUERY_FIELDS}    
+
+   
+
+def main(params):
+    if ("DEALER" in params):
+        return {"query": query_by_dealership(params["DEALER"]), "include_docs":True};
+    elif ("ID" in params):
+        return {"query": query_by_id(params["ID"]), "include_docs":True};
+    else:
+        return {"query": query_all(), "include_docs":True};
+
+main({"ID":3})
